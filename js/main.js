442d86b4f865bdb1072edac3a4c88d8756e2e3d4
Nahid-07 <nahidasif1998@gmail.com>
2022-08-30 12:39:18 +0600

    added show more button

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-const loadData = async(id) =>{
+const loadData = async(id,dataLimit) =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${id}`;
     const res = await fetch(url)
     const data = await res.json()
-    displayData(data.data)
+    displayData(data.data,dataLimit)
 }
 // display data on the UI
-const displayData = (phones) => {
+const displayData = (phones,dataLimit) => {
     console.log(phones);
 
     // show more button implement
 
     const btnAll = document.getElementById('btn-show-more');
-    if(phones.length > 12){
+    if(dataLimit && phones.length > 12){
         phones = phones.slice(0,12);
         btnAll.classList.remove('d-none')
     }else{
@@ -38,29 +38,29 @@ const displayData = (phones) => {
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
                 additional content. This content is a little bit longer.</p>
-                <button onclick="showMoreData()" href="#" class="btn btn-primary">Go somewhere</button>
+                <button href="#" class="btn btn-primary">Go somewhere</button>
+            </div>
         </div>
-    </div>
     `;
     phoneContainer.appendChild(div);
     })
     loading(false)
 }
 // common function search
-const searchProcess = ()=>{
+const searchProcess = (dataLimit)=>{
     loading(true)
     const searchfield = document.getElementById('search-field');
     const searchInput = searchfield.value;
-    loadData(searchInput);
+    loadData(searchInput,dataLimit);
 }
 // search button
 document.getElementById('search-button').addEventListener('click', () => {
-        searchProcess();
+        searchProcess(10);
 })
 // search with hit enter key
 document.getElementById('search-field').addEventListener('keypress',(event) => {
     if(event.key === 'Enter'){
-        searchProcess();
+        searchProcess(10);
     }
 })
 // loader function
@@ -72,3 +72,7 @@ const loading = (isLoading) => {
         loader.classList.add('d-none');
     }
 }
+// show rest button
+document.getElementById('btn-show-more').addEventListener('click',()=>{
+    searchProcess();
+})
